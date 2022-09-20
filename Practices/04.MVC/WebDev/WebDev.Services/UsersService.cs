ebfd628be90d74da6d8f476302767cbb0979c028
Moriarty <cristian_gira@hotmail.com>
2022-09-20 00:20:02 -0500

    practicas

---
+++ Practices/04.MVC/WebDev/WebDev.Services/UsersService.cs
@@ -0,0 +1,16 @@
+﻿namespace WebDev.Services
+{
+    public class UsersService
+    {
+        private string BaseUrl { get; }
+
+        private HttpClient _httpClient;
+        public UsersService(string baseUrl)
+        {
+            BaseUrl = baseUrl;
+        }
+
+
+
+    }
+}
